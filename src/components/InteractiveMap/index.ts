35b8cec28ca2be73ab2fee3f931376b47c670301
rafaelrtmatos-stm <rafaelrtmatos@gmail.com>
2026-05-22 19:36:20 -0300

    feat: Add barrel export for InteractiveMap components

---
+++ src/components/InteractiveMap/index.ts
@@ -0,0 +1,11 @@
+export { InteractiveMap } from './InteractiveMap';
+export type { InteractiveMapProps, MapMarker } from './InteractiveMap';
+export { MapControls } from './MapControls';
+export { Marker, drawMarkersOnCanvas } from './Marker';
+export { SplashScreen } from './SplashScreen';
+export { useMapScaling } from './useMapScaling';
+export {
+  exportMapAsImage,
+  exportMapAsPDF,
+  exportMapHighResolution,
+} from './ExportUtils';
